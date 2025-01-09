fa80b2c89d7cc960358a72d3817362ddda5f4ddf
sotaroun <acesoichi@yahoo.co.jp>
2025-01-09 22:40:20 +0900

    streamlitapp

--- abeapp.py
+++ abeapp.py
@@ -1,8 +1,60 @@
 import streamlit as st
+from selenium import webdriver
 from PIL import Image
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome import service as fs
+from selenium.webdriver import ChromeOptions
+from webdriver_manager.core.os_manager import ChromeType
+from selenium.webdriver.common.by import By
 
-st.title('アベアプリ')
+# タイトル
+st.title('ディズニーグッズ情報')
+
+# ボタンを作成(このボタンをアプリ上で押すと"if press_button:"より下の部分が実行される)
+
+press_button = st.button("スクレイピング開始")
+
+if press_button:
+    # スクレイピングするwebサイトのURL
+    URL = "https://www.tokyodisneyresort.jp/tdl/shop.html"
+
+    # ドライバのオプション
+    options = ChromeOptions()
+
+    # option設定を追加
+    options.add_argument("--headless")
+    options.add_argument('--disable-gpu')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+
+    # webdriver_managerによりドライバーをインストール
+    # chromiumを使用したいのでchrome_type引数でchromiumを指定しておく
+    CHROMEDRIVER = ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
+    service = fs.Service(CHROMEDRIVER)
+    driver = webdriver.Chrome(
+                                options=options,
+                                service=service
+                                 )
+# URLで指定したwebページを開く
+driver.get(URL)
+
+# webページ上のタイトル画像を取得
+img = driver.find_element(By.TAG_NAME, 'img')
+src = img.get_attribute('src')
+
+# 取得した画像をカレントディレクトリに保存
+with open(f"tmp_img.png", "wb") as f:
+    f.write(img.screenshot_as_png)
+
+# 保存した画像をstreamlitアプリ上に表示
+st.image("tmp_img.png")
+
+# webページを閉じる
+driver.close()
+
+# スクレイピングが完了したことをstreamlitアプリ上に表示する
+st.write("スクレイピング完了！")
+
 st.caption('これは安部のテストアプリです')
 
-image = Image.open('./data/abeimage.webp')
-st.image(image, width=200)
+
